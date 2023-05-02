111389895226f76d87dd4da6f9bf3d1c047f8f0f
Alexander Luque Hoffrogge <alexanderluquehoffrogge@gmail.com>
2023-05-02 14:29:40 +0200

    Probando con los modals para los formularios

--- ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/SocioController.java
+++ ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/SocioController.java
@@ -3,8 +3,12 @@ package com.salesianos.triana.dam.clubDeportivo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.salesianos.triana.dam.clubDeportivo.model.Socio;
 import com.salesianos.triana.dam.clubDeportivo.service.SocioService;
 
 @Controller
@@ -20,5 +24,14 @@ public class SocioController {
 		model.addAttribute("socios", service.findAll());
 		
 		return "html/panelAdmin";
+	}
+	
+	@PostMapping("/panel-admin/add-socio")
+	public String addSocio(@ModelAttribute("formAddSocio") Socio socio, BindingResult result) {
+		if (result.hasErrors()) {
+		    return "html/panelAdmin";
+		  }
+		  // procesar datos del formulario y redirigir a otra página
+		  return "html/panelAdmin";
 	}
 }
