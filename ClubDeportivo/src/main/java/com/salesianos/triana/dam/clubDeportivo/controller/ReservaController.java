b1fe2bfe7d18d5554dbb149712ed840ad273c032
Alex <alexanderluquehoffrogge@gmail.com>
2023-05-07 16:16:09 +0200

    Commit para guardar bd

--- ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/ReservaController.java
+++ ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/ReservaController.java
@@ -1,16 +1,20 @@
 package com.salesianos.triana.dam.clubDeportivo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.salesianos.triana.dam.clubDeportivo.model.Reserva;
 import com.salesianos.triana.dam.clubDeportivo.service.DeporteService;
 import com.salesianos.triana.dam.clubDeportivo.service.PistaService;
 import com.salesianos.triana.dam.clubDeportivo.service.ReservaService;
+import com.salesianos.triana.dam.clubDeportivo.service.SocioService;
 
 @Controller
 public class ReservaController {
@@ -21,13 +25,24 @@ public class ReservaController {
 	private DeporteService deporteService;
 	@Autowired
 	private PistaService pistaService;
+	@Autowired
+	private SocioService socioService;
 
 	@GetMapping("panel-admin/reservas")
-	public String listarReservas(Model model) {
+	public String listarReservas(Model model, @RequestParam(required = false) Long reservaId) {
 
 		model.addAttribute("reservas", service.findAll());
-		
-		return "panelAdmin";
+		model.addAttribute("deportes", deporteService.findAll());
+	    model.addAttribute("pistas", pistaService.findAll());
+	    model.addAttribute("socios", socioService.findAll());
+	    if (reservaId != null) {
+	        Optional<Reserva> reserva = service.findById(reservaId);
+	        model.addAttribute("reserva", reserva);
+	    } else {
+	        model.addAttribute("reserva", new Reserva());
+	    }
+
+	    return "panelAdmin";
 	}
 	@PostMapping("/addReservas")
 	public String agregarReservaAdmin(@ModelAttribute("reserva") Reserva reserva) {
