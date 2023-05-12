2eaacc3a5cac48c7f17363942b85205d5d883edc
Alexander Luque Hoffrogge <alexanderluquehoffrogge@gmail.com>
2023-05-12 13:36:48 +0200

    Añadida atenticathion en formulario para que detecte el socio pero no parece que la asocie bien cuando se hace la reserva

--- ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/ReservaController.java
+++ ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/ReservaController.java
@@ -1,6 +1,7 @@
 package com.salesianos.triana.dam.clubDeportivo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianos.triana.dam.clubDeportivo.model.Reserva;
+import com.salesianos.triana.dam.clubDeportivo.model.Socio;
 import com.salesianos.triana.dam.clubDeportivo.service.DeporteService;
 import com.salesianos.triana.dam.clubDeportivo.service.PistaService;
 import com.salesianos.triana.dam.clubDeportivo.service.ReservaService;
+import com.salesianos.triana.dam.clubDeportivo.service.SocioService;
 
 @Controller
 public class ReservaController {
@@ -21,15 +24,17 @@ public class ReservaController {
 	private DeporteService deporteService;
 	@Autowired
 	private PistaService pistaService;
-	//@Autowired
-	//private SocioService socioService;
+	@Autowired
+	private SocioService socioService;
 
 	@GetMapping("/reserva-pista")
-	public String verFormularioReserva(Model model) {
-		model.addAttribute("pistas", pistaService.findAll());
-		model.addAttribute("deportes", deporteService.findAll());
-		model.addAttribute("reserva", new Reserva());
-		return "formularioReserva";
+	public String verFormularioReserva(Model model, @AuthenticationPrincipal Socio s) {
+	    Reserva reserva = new Reserva();
+	    reserva.setSocio(s);
+	    model.addAttribute("pistas", pistaService.findAll());
+	    model.addAttribute("deportes", deporteService.findAll());
+	    model.addAttribute("reserva", reserva);
+	    return "formularioReserva";
 	}
 	
 	@PostMapping("/reserva-pista/nuevo")
