d7fe67d78fe3b80a4a03d33408b54d10b70a0c72
Alex <alexanderluquehoffrogge@gmail.com>
2023-05-18 19:14:57 +0200

    Funcionalidades hechas. Se ha añadido también en Socio que ordene por id pero no funciona, hay que mirarlo

--- ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/DeporteController.java
+++ ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/DeporteController.java
@@ -1,25 +1,9 @@
 package com.salesianos.triana.dam.clubDeportivo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.salesianos.triana.dam.clubDeportivo.model.Deporte;
-import com.salesianos.triana.dam.clubDeportivo.service.DeporteService;
 
 @Controller
 public class DeporteController {
 
-	@Autowired
-	private DeporteService service;
-	
-	@GetMapping("/panel-admin/deportes")
-	public String listarDeportes(Model model) {
 
-		model.addAttribute("deportes", service.findAll());
-		model.addAttribute("deporte", new Deporte());
-
-		return "panelAdmin";
-	}
 }
