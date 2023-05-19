1f6d83b12924ca99e0bfdd830c0722dbb4e9218d
Alex <alexanderluquehoffrogge@gmail.com>
2023-05-19 20:14:45 +0200

    Horario hecho, me falta comprobar que el usuario no pueda acceder a días anteriores, poner las horas pasadas para que no se puedan pulsar, hacer varias comprobaciones y que las disponibles vayan al fomrulario

--- ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/ReservaController.java
+++ ClubDeportivo/src/main/java/com/salesianos/triana/dam/clubDeportivo/controller/ReservaController.java
@@ -1,12 +1,12 @@
 package com.salesianos.triana.dam.clubDeportivo.controller;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.salesianos.triana.dam.clubDeportivo.model.Deporte;
 import com.salesianos.triana.dam.clubDeportivo.model.Reserva;
 import com.salesianos.triana.dam.clubDeportivo.model.Socio;
 import com.salesianos.triana.dam.clubDeportivo.service.DeporteService;
@@ -34,41 +35,88 @@ public class ReservaController {
 	@Autowired
 	private EmailSenderService emailService;
 
+	/*
+	 * @GetMapping("/disponibilidad") public String mostrarReservasCalendario(Model
+	 * model, @RequestParam(defaultValue = "1") int idDeporte) { //meter tambien dia
+	 * int numeroPistas =
+	 * deporteService.findById(idDeporte).get().getPistas().size(); int numeroHoras
+	 * = 15; List<Integer> pistas = new ArrayList<>(); List<Reserva> reservas =
+	 * service.findReservasHoyYDeporte(idDeporte); LocalDate hoy=LocalDate.now();
+	 * List<LocalTime> horas = new ArrayList<>(); LocalTime horaInicial =
+	 * LocalTime.of(7, 0);
+	 * 
+	 * for (int i = 0; i < numeroHoras; i++) { horas.add(horaInicial.plusHours(i));
+	 * }
+	 * 
+	 * for (int i = 0; i < numeroPistas; i++) { int pista = i + 1;
+	 * pistas.add(pista); }
+	 * 
+	 * model.addAttribute("pistas", pistaService.findAll());
+	 * model.addAttribute("reservas", reservas); model.addAttribute("horas", horas);
+	 * model.addAttribute("pistas", pistas); model.addAttribute("idDeporte",
+	 * idDeporte); model.addAttribute("dia", hoy); return "disponibilidad"; }
+	 */
+
 	@GetMapping("/disponibilidad")
-	public String mostrarReservasCalendario(Model model, @RequestParam(defaultValue = "1") int idDeporte) { //meter tambien dia
-		int numeroPistas = deporteService.findById(idDeporte).get().getPistas().size();
+	public String mostrarReservasCalendario(Model model, @RequestParam(defaultValue = "1") int idDeporte,
+			@RequestParam(defaultValue = "#{T(java.time.LocalDate).now()}") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dia) {
+		Deporte deporte = deporteService.findById(idDeporte).get();
 		int numeroHoras = 15;
-		List<Integer> pistas = new ArrayList<>();
+		LocalDate diaSiguiente = dia.plusDays(1);
+		LocalDate diaAnterior = dia.minusDays(1);
 		List<Reserva> reservas = service.findReservasHoyYDeporte(idDeporte);
-		LocalDate hoy=LocalDate.now();
 		List<LocalTime> horas = new ArrayList<>();
 		LocalTime horaInicial = LocalTime.of(7, 0);
-		
+
+		// Crear matriz bidimensional de pistas y horas
+		boolean[][] pistasHoras = new boolean[numeroHoras][deporte.getPistas().size()];
+
+		// Marcar las pistas ocupadas en cada hora
+		for (Reserva reserva : reservas) {
+			int horaIndex = reserva.getHora_reserva().getHour() - horaInicial.getHour();
+			int pistaIndex = reserva.getPista().getId() - 1;
+			pistasHoras[horaIndex][pistaIndex] = true;
+		}
+
 		for (int i = 0; i < numeroHoras; i++) {
 			horas.add(horaInicial.plusHours(i));
 		}
-		
-		for (int i = 0; i < numeroPistas; i++) {
-			int pista = i + 1;
-			pistas.add(pista);
+
+		if (LocalDate.now().isAfter(diaAnterior)) {
+			model.addAttribute("mostrarBotonAnterior", false);
+		} else {
+			model.addAttribute("mostrarBotonAnterior", true);
 		}
-		
+
+		if (LocalDate.now().plusDays(7).isEqual(diaSiguiente) || LocalDate.now().plusDays(7).isBefore(diaSiguiente)) {
+			model.addAttribute("mostrarBotonSiguiente", false);
+		} else {
+			model.addAttribute("mostrarBotonSiguiente", true);
+		}
+
+		model.addAttribute("deporte", deporte);
 		model.addAttribute("pistas", pistaService.findAll());
+		model.addAttribute("deportes", deporteService.findAll());
 		model.addAttribute("reservas", reservas);
 		model.addAttribute("horas", horas);
-		model.addAttribute("pistas", pistas);
+		model.addAttribute("pistas", deporte.getPistas());
 		model.addAttribute("idDeporte", idDeporte);
-		model.addAttribute("dia", hoy);
+		model.addAttribute("hoy", LocalDate.now());
+		model.addAttribute("dia", dia);
+		model.addAttribute("pistasHoras", pistasHoras);
+		model.addAttribute("diaSiguiente", diaSiguiente);
+		model.addAttribute("diaAnterior", diaAnterior);
+
 		return "disponibilidad";
 	}
-	
+
 	@GetMapping("/reserva-pista")
 	public String verFormularioReserva(Model model) {
 		Reserva reserva = new Reserva();
 		model.addAttribute("pistas", pistaService.findAll());
 		model.addAttribute("deportes", deporteService.findAll());
 		model.addAttribute("reserva", reserva);
-		//model.addAttribute("reservaExitosa", false);
+		// model.addAttribute("reservaExitosa", false);
 		return "formularioReserva";
 	}
 
